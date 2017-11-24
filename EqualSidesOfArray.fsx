2e2ca28875a9e535c35cabed9b7d55542da829c8
Attie Naude <attie.naude@pipware.net>
2017-11-24 09:31:04 +0200

    Adds another solution

---
+++ EqualSidesOfArray.fsx
@@ -0,0 +1,15 @@
+﻿open System
+
+let indexIsEven (items: int array) (index: int) =
+    let left = items |> Array.take index |> Array.append [|0|]
+    let right = items |> Array.skip (Math.Min(index + 1, items.Length)) |> Array.append [|0|]
+    Array.sum left = Array.sum right
+
+let findEvenIndex (items : int array) = 
+    let evenIndices = 
+        [|0..items.Length|]
+        |> Array.filter (indexIsEven items)
+    
+    if evenIndices.Length = 0 then -1 else Seq.min evenIndices
+
+findEvenIndex [|1; 2; 3; 4; 3; 2; 1|]
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# My personal scratchpad for [CodeWars](www.codewars.com) katas
+# My personal scratchpad for [CodeWars](www.codewars.com) challenges
 
 ## Completed katas:
 * [Rotate for a Max](https://www.codewars.com/kata/56a4872cbb65f3a610000026)
+* [Equal Sides Of An Array](https://www.codewars.com/kata/equal-sides-of-an-array/train/fsharp)
